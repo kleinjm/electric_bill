58a49ea9d71dbd5abda701dfe741e555f88d4aa8
James Klein <kleinjm007@gmail.com>
2020-06-29 08:57:38 -0600

    bill parsing working

--- app/services/parse_bill.rb
+++ app/services/parse_bill.rb
@@ -5,8 +5,11 @@ require "pdf-reader"
 class ParseBill
   def call
     reader = PDF::Reader.new(Rails.root.join("tmp/last_bill.pdf"))
-    reader.pages.first.text.split("\n").select { |row| row.match?(/Electricity Service/) }.first
+    stats_row = reader.pages.first.text.split("\n").select { |row| row.match?(/Electricity Service/) }.first
+    words = stats_row.split(" ")
+    kwh = words[5].to_f
+    cost = words[7].gsub("$", "").to_f
+
+    (cost / kwh).round(2)
   end
 end
-
-ParseBill.new.call
